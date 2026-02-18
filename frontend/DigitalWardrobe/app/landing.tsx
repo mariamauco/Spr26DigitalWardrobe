f59cc59fecd6d4335d4b936879f03363606a51b5
samanthfel <samanthfel@gmail.com>
2026-02-17 20:57:01 -0500

    set up expo app and added landing screen

--- frontend/DigitalWardrobe/app/landing.tsx
+++ frontend/DigitalWardrobe/app/landing.tsx
@@ -5,9 +5,9 @@ export default function LandingScreen() {
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Welcome to DigitalWardrobe</Text>
-		<Link href='/signUp'>Go to sign up</Link>
-        <Link href='/logIn'>Go to log in</Link>
-        <Link href='/dashboard'>Go to Dashboard</Link>
+			<Link href="/signUp" style={styles.link}>Go to sign up</Link>
+			<Link href="/logIn" style={styles.link}>Go to log in</Link>
+			<Link href="/dashboard" style={styles.link}>Go to Dashboard</Link>
         </View>
 	);
 }
@@ -15,13 +15,21 @@ export default function LandingScreen() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: "#fff",
 		alignItems: 'center',
 		justifyContent: 'center',
 		padding: 24,
+		gap: 12,
 	},
 	title: {
 		fontSize: 24,
 		fontWeight: '600',
 		textAlign: 'center',
+		color: "#111",
+		marginBottom: 8,
 	},
+	link: {
+		color: "#2563eb",
+		fontSize: 16,
+	}
 });
