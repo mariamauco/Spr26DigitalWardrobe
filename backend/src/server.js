4d2081a03acd07d82fed7ac683ed79f8e4dd9ead
beanavas <beanavasaguilera@gmail.com>
2026-02-19 16:22:50 -0500

    Created routes for uploading clothes, created bare frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import authRoutes from "./routes/auth.js"; 
+import path from "path";
+import clothingRoutes from "./routes/clothing.js";
 
 const app = express();
 app.use(cors()); // allows front end to call backend
@@ -27,5 +29,11 @@ mongoose.connect(process.env.MONGO_URI)
   })
   .catch((err) => console.error("Mongo connection error:", err));
 
+app.use(cors({ origin: "http://localhost:5173" })); // or "*" for quick testing
 app.use("/api/auth", authRoutes);  
 
+// UPLOADING IMAGES //
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+// CLOTHING ROUTES //
+app.use("/api/clothing", clothingRoutes);
