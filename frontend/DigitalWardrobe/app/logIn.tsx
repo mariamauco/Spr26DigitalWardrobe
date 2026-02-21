aad4a93234dc1147cdd483d10db718b6e7789706
mariamauco <mariafmauco@gmail.com>
2026-02-20 23:33:57 -0500

    Add UI from index.html to Dashboard, LogIn, and SignUp screens using tsx

--- frontend/DigitalWardrobe/app/logIn.tsx
+++ frontend/DigitalWardrobe/app/logIn.tsx
@@ -1,10 +1,20 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { TextInput ,Text, View, StyleSheet } from 'react-native';
+
 
 export default function LogInScreen() {
 	return (
+		<>
 		<View style={styles.container}>
 			<Text style={styles.title}>Log In</Text>
 		</View>
+		<View style={{padding:3, margin:5, flex:4, alignItems:'center'}}>
+			<TextInput id='email' placeholder='email' style={styles.input}/>
+			<TextInput id='password' placeholder='password' style={styles.input}/>
+			<button id='loginBtn'>Login</button>
+		</View>
+		
+		</>
+		
 	);
 }
 
@@ -19,4 +29,12 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: '600',
 	},
+	input:{
+		width:'50%', 
+		borderColor:'black', 
+		borderWidth:1, 
+		margin:3,
+		borderRadius:5,
+		padding:1,
+	}
 });
--- frontend/DigitalWardrobe/app/signUp.tsx
+++ frontend/DigitalWardrobe/app/signUp.tsx
@@ -1,10 +1,17 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 export default function SignUpScreen() {
 	return (
+		<>
 		<View style={styles.container}>
 			<Text style={styles.title}>Sign Up</Text>
+		</View><View style={{ padding: 3, margin: 5, flex: 4, alignItems: 'center' }}>
+				<TextInput id='name' placeholder='name' style={styles.input} />
+				<TextInput id='email' placeholder='email' style={styles.input} />
+				<TextInput id='password' placeholder='password' style={styles.input} />
+				<button id='loginBtn'>Sign Up</button>
 		</View>
+		</>
 	);
 }
 
@@ -19,4 +26,12 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: '600',
 	},
+	input:{
+		width:'50%', 
+		borderColor:'black', 
+		borderWidth:1, 
+		margin:3,
+		borderRadius:5,
+		padding:1,
+	}
 });
