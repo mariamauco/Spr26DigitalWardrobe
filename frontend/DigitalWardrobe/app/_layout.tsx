d2211b25dd8b64d833c18946e465e6d0f04a0276
Gibber17 <gfrancis1707@gmail.com>
2026-02-24 14:44:49 -0500

    Created HTTPS POST request to send signUp and LogIn info to backend

--- frontend/DigitalWardrobe/app/_layout.tsx
+++ frontend/DigitalWardrobe/app/_layout.tsx
@@ -11,12 +11,13 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={DefaultTheme}>
       <Stack initialRouteName="landing">
         <Stack.Screen name="landing" options={{ title: 'Landing' }} />
         <Stack.Screen name="dashboard" options={{ title: 'Dashboard' }} />
         <Stack.Screen name="logIn" options={{ title: 'Log In' }} />
         <Stack.Screen name="signUp" options={{ title: 'Sign Up' }} />
+        <Stack.Screen name="onboarding" options={{ title: 'Onboarding' }} />
 
       </Stack>
       <StatusBar style="auto" />
--- frontend/DigitalWardrobe/app/logIn.tsx
+++ frontend/DigitalWardrobe/app/logIn.tsx
@@ -1,16 +1,59 @@
 import { TextInput ,Text, View, StyleSheet } from 'react-native';
-
+import React, { useState } from "react";
+import { Pressable } from "react-native";
+import { Alert } from "react-native";
 
 export default function LogInScreen() {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");	
+
+	const login = async () => {
+
+		// check to see if user entered in all values
+		if (!email || !password) {
+			Alert.alert("Error", "All fields are required");
+			return;
+		}
+
+		const payload = {
+			email,
+			password
+		};
+
+		try {
+		const response = await fetch("http://138.197.16.179:5050/", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(payload),
+		});
+
+		if (!response.ok) {
+		throw new Error("Login failed");
+		}
+
+		const data = await response.json();
+		Alert.alert("Success", "Login successful!"); // sends notification to user that login was succesful
+
+		} catch (error) {
+		Alert.alert("Error", "Signup failed. Please try again."); // sends notification to user that login was NOT succesful
+		console.error("Error:", error);
+		}
+	};
+	
 	return (
 		<>
 		<View style={styles.container}>
 			<Text style={styles.title}>Log In</Text>
 		</View>
 		<View style={{padding:3, margin:5, flex:4, alignItems:'center'}}>
-			<TextInput id='email' placeholder='email' style={styles.input}/>
-			<TextInput id='password' placeholder='password' style={styles.input}/>
-			<button id='loginBtn'>Login</button>
+			<TextInput placeholder='email' style={styles.input} value={email} onChangeText={setEmail}/>
+			<TextInput placeholder='password' style={styles.input} secureTextEntry value={password} onChangeText={setPassword}/>
+			<Pressable onPress={login}>
+				<Text>Log In</Text>
+			</Pressable>
 		</View>
 		
 		</>
