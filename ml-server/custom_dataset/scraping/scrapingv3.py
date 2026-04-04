f9e89c3d16cc452fdb2193387ce87c9a3816bbe6
mariamauco <mariafmauco@gmail.com>
2026-04-04 14:53:07 -0400

    scraping script to use selenium

--- ml-server/custom_dataset/scraping/scrapingv3.py
+++ ml-server/custom_dataset/scraping/scrapingv3.py
@@ -8,12 +8,17 @@ to do this: install selenium `pip install selenium`
 '''
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import time, os
 
 def scrapeBoard(url):
 
     # Open a browser in the background
     options = webdriver.ChromeOptions()
+
+    # 1. Define a folder in your current directory to save the session
+    profile_path = os.path.join(os.getcwd(), "pinterest_profile")
+    options.add_argument(f"user-data-dir={profile_path}")
+
     driver = webdriver.Chrome(options=options)
 
     # open the board and let the page load
@@ -21,7 +26,7 @@ def scrapeBoard(url):
     time.sleep(3)
 
     pinIDs = set() # set to prevent dupes
-    scroll = driver.execute_script("return document.body.scrollHeight")
+    height = driver.execute_script("return document.body.scrollHeight")
 
     # start scrolling!
 
@@ -32,9 +37,9 @@ def scrapeBoard(url):
         pins = driver.find_elements(By.CSS_SELECTOR, "a[href^='/pin/']")
 
         for pin in pins:
-            href = el.get_attribute('href')
+            href = pin.get_attribute('href')
             if href: # only add the id
-                pin_ids.add(href.split('/pin/')[1].strip('/'))
+                pinIDs.add(href.split('/pin/')[1].strip('/'))
 
         # scroll down and wait
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -48,8 +53,8 @@ def scrapeBoard(url):
         height = new_height
     driver.quit()
 
-    print(f"\nTotal unique pins found: {len(pin_ids)}")
-    return list(pin_ids)
+    print(f"\nTotal unique pins found: {len(pinIDs)}")
+    return list(pinIDs)
 
 
 board_url = "https://www.pinterest.com/imbervintage/y2k-aesthetic/"
